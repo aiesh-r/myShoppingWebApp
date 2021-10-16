a8fd367498f45d520d62e85c55e7ddf5c8b52649
aiesh-r <ishani.rangana123@gmail.com>
2021-10-16 14:07:46 +0530

    Feat(Header) - Add header feature

--- src/app/themes/themes.module.ts
+++ src/app/themes/themes.module.ts
@@ -4,10 +4,19 @@ import { RouterModule } from '@angular/router';
 import { AdminLayoutComponent } from './admin-layout/admin-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [AdminLayoutComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   exports: [],
 })
 export class ThemesModule {}
